5b46e35d452f073ab0a6506c5dcd9c922e5213cd
Arne Vansteenkiste <barnex@gmail.com>
2015-05-20 15:38:02 +0200

    add util.go

---
+++ util.go
@@ -0,0 +1,47 @@
+package vinamax2
+
+// common utilities
+
+import (
+	"log"
+	"path"
+	"runtime"
+)
+
+// Remove extension from file name.
+func NoExt(file string) string {
+	ext := path.Ext(file)
+	return file[:len(file)-len(ext)]
+}
+
+// If err != nil, trigger log.Fatal(msg, err)
+func FatalErr(err interface{}) {
+	_, file, line, _ := runtime.Caller(1)
+	if err != nil {
+		log.Fatal(file, ":", line, err)
+	}
+}
+
+// Panics if err is not nil. Signals a bug.
+func PanicErr(err error) {
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+func Log(msg ...interface{}) {
+	log.Println(msg...)
+}
+
+// Logs the error of non-nil, plus message
+func LogErr(err error, msg ...interface{}) {
+	if err != nil {
+		log.Println(append(msg, err)...)
+	}
+}
+
+func assert(test bool) {
+	if !test {
+		panic("assertion failed")
+	}
+}
