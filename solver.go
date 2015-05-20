9930f2639c51bc3f55430375fbcfb8eb86825a4d
JLeliaert <jonathan.leliaert@gmail.com>
2015-05-20 14:12:48 +0200

    deleted fehls in solver.go

--- solver.go
+++ solver.go
@@ -1,7 +1,11 @@
-package main
-
-import ()
-
+//package vinamax
+//
+//import (
+//	//		"fmt"
+//	"log"
+//	"math"
+//)
+//
 ////Set the solver to use, "euler" or "heun"
 //func Setsolver(a string) {
 //	switch a {
@@ -66,7 +70,7 @@ import ()
 //	testinput()
 //	syntaxrun()
 //	for i := range universe.lijst {
-//		norm(universe.lijst[i].m)
+//		norm(universe.lijst[i].M)
 //	}
 //	write(averagemoments(universe.lijst))
 //	//averages is not weighted with volume, averagemoments is
@@ -229,7 +233,7 @@ import ()
 //		temp := p.temp()
 //		p.tempfield = temp
 //		tau1 := p.tau(temp)
-//		p.fehlk1 = tau1
+//		p.k1 = tau1
 //
 //		//tau (t+1)
 //		p.M[0] += tau1[0] * Dt
@@ -246,7 +250,7 @@ import ()
 //		temp := p.tempfield
 //		//temp := p.temp()
 //		tau2 := p.tau(temp)
-//		tau1 := p.fehlk1
+//		tau1 := p.k1
 //		p.M[0] += ((-tau1[0] + tau2[0]) * 0.5 * Dt)
 //		p.M[1] += ((-tau1[1] + tau2[1]) * 0.5 * Dt)
 //		p.M[2] += ((-tau1[2] + tau2[2]) * 0.5 * Dt)
@@ -277,7 +281,7 @@ import ()
 //		temp := p.temp()
 //		p.tempfield = temp
 //		tau0 := p.tau(temp)
-//		p.fehlk1 = tau0
+//		p.k1 = tau0
 //
 //		//k1
 //		p.M[0] += tau0[0] * 1 / 2. * Dt
@@ -293,8 +297,8 @@ import ()
 //	for _, p := range Lijst {
 //		temp := p.tempfield
 //		k2 := p.tau(temp)
-//		p.fehlk2 = k2
-//		k1 := p.fehlk1
+//		p.k2 = k2
+//		k1 := p.k1
 //		p.M[0] += ((-3/2.*k1[0] + 2*k2[0]) * Dt)
 //		p.M[1] += ((-3/2.*k1[1] + 2*k2[1]) * Dt)
 //		p.M[2] += ((-3/2.*k1[2] + 2*k2[2]) * Dt)
@@ -306,8 +310,8 @@ import ()
 //	for _, p := range Lijst {
 //		temp := p.tempfield
 //		k3 := p.tau(temp)
-//		k1 := p.fehlk1
-//		k2 := p.fehlk2
+//		k1 := p.k1
+//		k2 := p.k2
 //		p.M[0] += ((7/6.*k1[0] - 4/3.*k2[0] + 1/6.*k3[0]) * Dt)
 //		p.M[1] += ((7/6.*k1[1] - 4/3.*k2[1] + 1/6.*k3[1]) * Dt)
 //		p.M[2] += ((7/6.*k1[2] - 4/3.*k2[2] + 1/6.*k3[2]) * Dt)
@@ -338,7 +342,7 @@ import ()
 //		temp := p.temp()
 //		p.tempfield = temp
 //		tau0 := p.tau(temp)
-//		p.fehlk1 = tau0
+//		p.k1 = tau0
 //
 //		//k1
 //		p.M[0] += tau0[0] * 1. / 10. * Dt
@@ -354,8 +358,8 @@ import ()
 //	for _, p := range Lijst {
 //		temp := p.tempfield
 //		k2 := p.tau(temp)
-//		p.fehlk2 = k2
-//		k1 := p.fehlk1
+//		p.k2 = k2
+//		k1 := p.k1
 //		p.M[0] += (((-1./10.-2189./5746.)*k1[0] + 2310./2873.*k2[0]) * Dt)
 //		p.M[1] += (((-1./10.-2189./5746.)*k1[1] + 2310./2873.*k2[1]) * Dt)
 //		p.M[2] += (((-1./10.-2189./5746.)*k1[2] + 2310./2873.*k2[2]) * Dt)
@@ -367,8 +371,8 @@ import ()
 //	for _, p := range Lijst {
 //		temp := p.tempfield
 //		k3 := p.tau(temp)
-//		k1 := p.fehlk1
-//		k2 := p.fehlk2
+//		k1 := p.k1
+//		k2 := p.k2
 //		p.M[0] += (((89./33.+2189./5746.)*k1[0] + (-475./126-2310./2873.)*k2[0] + 2873/1386.*k3[0]) * Dt)
 //		p.M[1] += (((89./33.+2189./5746.)*k1[1] + (-475./126-2310./2873.)*k2[1] + 2873/1386.*k3[1]) * Dt)
 //		p.M[2] += (((89./33.+2189./5746.)*k1[2] + (-475./126-2310./2873.)*k2[2] + 2873/1386.*k3[2]) * Dt)
@@ -400,7 +404,7 @@ import ()
 //		temp := p.temp()
 //		tau0 := p.tau(temp)
 //		p.tempfield = temp
-//		p.fehlk1 = tau0
+//		p.k1 = tau0
 //
 //		//k1
 //		p.M[0] += tau0[0] * 1 / 2. * Dt
@@ -416,8 +420,8 @@ import ()
 //	for _, p := range Lijst {
 //		temp := p.tempfield
 //		k2 := p.tau(temp)
-//		p.fehlk2 = k2
-//		k1 := p.fehlk1
+//		p.k2 = k2
+//		k1 := p.k1
 //		p.M[0] += ((-1/2.*k1[0] + 1/2.*k2[0]) * Dt)
 //		p.M[1] += ((-1/2.*k1[1] + 1/2.*k2[1]) * Dt)
 //		p.M[2] += ((-1/2.*k1[2] + 1/2.*k2[2]) * Dt)
@@ -428,8 +432,8 @@ import ()
 //	for _, p := range Lijst {
 //		temp := p.tempfield
 //		k3 := p.tau(temp)
-//		p.fehlk3 = k3
-//		k2 := p.fehlk2
+//		p.k3 = k3
+//		k2 := p.k2
 //		p.M[0] += ((-1/2.*k2[0] + 1*k3[0]) * Dt)
 //		p.M[1] += ((-1/2.*k2[1] + 1*k3[1]) * Dt)
 //		p.M[2] += ((-1/2.*k2[2] + 1*k3[2]) * Dt)
@@ -441,9 +445,9 @@ import ()
 //	for _, p := range Lijst {
 //		temp := p.tempfield
 //		k4 := p.tau(temp)
-//		k1 := p.fehlk1
-//		k2 := p.fehlk2
-//		k3 := p.fehlk3
+//		k1 := p.k1
+//		k2 := p.k2
+//		k3 := p.k3
 //		p.M[0] += ((1/6.*k1[0] + 1/3.*k2[0] - 2/3.*k3[0] + 1/6.*k4[0]) * Dt)
 //		p.M[1] += ((1/6.*k1[1] + 1/3.*k2[1] - 2/3.*k3[1] + 1/6.*k4[1]) * Dt)
 //		p.M[2] += ((1/6.*k1[2] + 1/3.*k2[2] - 2/3.*k3[2] + 1/6.*k4[2]) * Dt)
@@ -479,7 +483,7 @@ import ()
 //		temp := p.temp()
 //		k1 := p.tau(temp)
 //		p.tempfield = temp
-//		p.fehlk1 = k1
+//		p.k1 = k1
 //
 //		p.M[0] += k1[0] * 1 / 5. * Dt
 //		p.M[1] += k1[1] * 1 / 5. * Dt
@@ -493,9 +497,9 @@ import ()
 //	for _, p := range Lijst {
 //
 //		temp := p.tempfield
-//		k1 := p.fehlk1
+//		k1 := p.k1
 //		k2 := p.tau(temp)
-//		p.fehlk2 = k2
+//		p.k2 = k2
 //
 //		p.M = p.tempm
 //		p.M[0] += ((3/40.*k1[0] + 9/40.*k2[0]) * Dt)
@@ -508,10 +512,10 @@ import ()
 //	}
 //	for _, p := range Lijst {
 //		temp := p.tempfield
-//		k1 := p.fehlk1
-//		k2 := p.fehlk2
+//		k1 := p.k1
+//		k2 := p.k2
 //		k3 := p.tau(temp)
-//		p.fehlk3 = k3
+//		p.k3 = k3
 //
 //		p.M = p.tempm
 //		p.M[0] += ((44/45.*k1[0] - 56/15.*k2[0] + 32/9.*k3[0]) * Dt)
@@ -524,11 +528,11 @@ import ()
 //	}
 //	for _, p := range Lijst {
 //		temp := p.tempfield
-//		k1 := p.fehlk1
-//		k2 := p.fehlk2
-//		k3 := p.fehlk3
+//		k1 := p.k1
+//		k2 := p.k2
+//		k3 := p.k3
 //		k4 := p.tau(temp)
-//		p.fehlk4 = k4
+//		p.k4 = k4
 //
 //		p.M = p.tempm
 //		p.M[0] += ((19372/6561.*k1[0] - 25360/2187.*k2[0] + 64448/6561.*k3[0] - 212/729.*k4[0]) * Dt)
@@ -541,12 +545,12 @@ import ()
 //	}
 //	for _, p := range Lijst {
 //		temp := p.tempfield
-//		k1 := p.fehlk1
-//		k2 := p.fehlk2
-//		k3 := p.fehlk3
-//		k4 := p.fehlk4
+//		k1 := p.k1
+//		k2 := p.k2
+//		k3 := p.k3
+//		k4 := p.k4
 //		k5 := p.tau(temp)
-//		p.fehlk5 = k5
+//		p.k5 = k5
 //
 //		p.M = p.tempm
 //		p.M[0] += ((9017/3168.*k1[0] - 355/33.*k2[0] + 46732/5247.*k3[0] + 49/176.*k4[0] - 5103/18656.*k5[0]) * Dt)
@@ -560,13 +564,13 @@ import ()
 //
 //	for _, p := range Lijst {
 //		temp := p.tempfield
-//		k1 := p.fehlk1
-//		k2 := p.fehlk2
-//		k3 := p.fehlk3
-//		k4 := p.fehlk4
-//		k5 := p.fehlk5
+//		k1 := p.k1
+//		k2 := p.k2
+//		k3 := p.k3
+//		k4 := p.k4
+//		k5 := p.k5
 //		k6 := p.tau(temp)
-//		p.fehlk6 = k6
+//		p.k6 = k6
 //
 //		p.M = p.tempm
 //		p.M[0] += ((35/384.*k1[0] + 0.*k2[0] + 500/1113.*k3[0] + 125/192.*k4[0] - 2187/6784.*k5[0] + 11/84.*k6[0]) * Dt)
@@ -580,14 +584,14 @@ import ()
 //
 //	for _, p := range Lijst {
 //		temp := p.tempfield
-//		k1 := p.fehlk1
-//		k2 := p.fehlk2
-//		k3 := p.fehlk3
-//		k4 := p.fehlk4
-//		k5 := p.fehlk5
-//		k6 := p.fehlk6
+//		k1 := p.k1
+//		k2 := p.k2
+//		k3 := p.k3
+//		k4 := p.k4
+//		k5 := p.k5
+//		k6 := p.k6
 //		k7 := p.tau(temp)
-//		p.fehlk7 = k7
+//		p.k7 = k7
 //
 //		p.tempm[0] += ((5179/57600.*k1[0] + 0.*k2[0] + 7571/16695.*k3[0] + 393/640.*k4[0] - 92097/339200.*k5[0] + 187/2100.*k6[0] + 1/40.*k7[0]) * Dt)
 //		p.tempm[1] += ((5179/57600.*k1[1] + 0.*k2[1] + 7571/16695.*k3[1] + 393/640.*k4[1] - 92097/339200.*k5[1] + 187/2100.*k6[1] + 1/40.*k7[1]) * Dt)
@@ -624,7 +628,7 @@ import ()
 //		temp := p.temp()
 //		p.tempfield = temp
 //		k1 := p.tau(temp)
-//		p.fehlk1 = k1
+//		p.k1 = k1
 //
 //		p.M[0] += k1[0] * 1 / 6. * Dt
 //		p.M[1] += k1[1] * 1 / 6. * Dt
@@ -638,9 +642,9 @@ import ()
 //	for _, p := range Lijst {
 //
 //		temp := p.tempfield
-//		k1 := p.fehlk1
+//		k1 := p.k1
 //		k2 := p.tau(temp)
-//		p.fehlk2 = k2
+//		p.k2 = k2
 //
 //		p.M = p.tempm
 //		p.M[0] += ((4/75.*k1[0] + 16/75.*k2[0]) * Dt)
@@ -653,10 +657,10 @@ import ()
 //	}
 //	for _, p := range Lijst {
 //		temp := p.tempfield
-//		k1 := p.fehlk1
-//		k2 := p.fehlk2
+//		k1 := p.k1
+//		k2 := p.k2
 //		k3 := p.tau(temp)
-//		p.fehlk3 = k3
+//		p.k3 = k3
 //
 //		p.M = p.tempm
 //		p.M[0] += ((5/6.*k1[0] - 8/3.*k2[0] + 5/2.*k3[0]) * Dt)
@@ -669,11 +673,11 @@ import ()
 //	}
 //	for _, p := range Lijst {
 //		temp := p.tempfield
-//		k1 := p.fehlk1
-//		k2 := p.fehlk2
-//		k3 := p.fehlk3
+//		k1 := p.k1
+//		k2 := p.k2
+//		k3 := p.k3
 //		k4 := p.tau(temp)
-//		p.fehlk4 = k4
+//		p.k4 = k4
 //
 //		p.M = p.tempm
 //		p.M[0] += ((-8/5.*k1[0] + 144/25.*k2[0] - 4.*k3[0] + 16/25.*k4[0]) * Dt)
@@ -686,12 +690,12 @@ import ()
 //	}
 //	for _, p := range Lijst {
 //		temp := p.tempfield
-//		k1 := p.fehlk1
-//		k2 := p.fehlk2
-//		k3 := p.fehlk3
-//		k4 := p.fehlk4
+//		k1 := p.k1
+//		k2 := p.k2
+//		k3 := p.k3
+//		k4 := p.k4
 //		k5 := p.tau(temp)
-//		p.fehlk5 = k5
+//		p.k5 = k5
 //
 //		p.M = p.tempm
 //		p.M[0] += ((361/320.*k1[0] - 18/5.*k2[0] + 407/128.*k3[0] - 11/80.*k4[0] + 55/128.*k5[0]) * Dt)
@@ -705,13 +709,13 @@ import ()
 //
 //	for _, p := range Lijst {
 //		temp := p.tempfield
-//		k1 := p.fehlk1
-//		k2 := p.fehlk2
-//		k3 := p.fehlk3
-//		k4 := p.fehlk4
-//		k5 := p.fehlk5
+//		k1 := p.k1
+//		k2 := p.k2
+//		k3 := p.k3
+//		k4 := p.k4
+//		k5 := p.k5
 //		k6 := p.tau(temp)
-//		p.fehlk6 = k6
+//		p.k6 = k6
 //
 //		p.M = p.tempm
 //		p.M[0] += ((-11/640.*k1[0] + 0.*k2[0] + 11/256.*k3[0] - 11/160.*k4[0] + 11/256.*k5[0] + 0.*k6[0]) * Dt)
@@ -725,14 +729,14 @@ import ()
 //
 //	for _, p := range Lijst {
 //		temp := p.tempfield
-//		k1 := p.fehlk1
-//		k2 := p.fehlk2
-//		k3 := p.fehlk3
-//		k4 := p.fehlk4
-//		k5 := p.fehlk5
-//		k6 := p.fehlk6
+//		k1 := p.k1
+//		k2 := p.k2
+//		k3 := p.k3
+//		k4 := p.k4
+//		k5 := p.k5
+//		k6 := p.k6
 //		k7 := p.tau(temp)
-//		p.fehlk7 = k7
+//		p.k7 = k7
 //
 //		p.M = p.tempm
 //		p.M[0] += ((93/640.*k1[0] + -18/5.*k2[0] + 803/256.*k3[0] - 11/160.*k4[0] + 99/256.*k5[0] + 0.*k6[0] + 1/1.*k7[0]) * Dt)
@@ -743,15 +747,15 @@ import ()
 //
 //	for _, p := range Lijst {
 //		temp := p.tempfield
-//		k1 := p.fehlk1
-//		k2 := p.fehlk2
-//		k3 := p.fehlk3
-//		k4 := p.fehlk4
-//		k5 := p.fehlk5
-//		k6 := p.fehlk6
-//		k7 := p.fehlk7
+//		k1 := p.k1
+//		k2 := p.k2
+//		k3 := p.k3
+//		k4 := p.k4
+//		k5 := p.k5
+//		k6 := p.k6
+//		k7 := p.k7
 //		k8 := p.tau(temp)
-//		p.fehlk8 = k8
+//		p.k8 = k8
 //
 //		p.M = p.tempm
 //		p.tempm[0] += ((31/384.*k1[0] + 0.*k2[0] + 1125/2816.*k3[0] + 9/32.*k4[0] + 125/768.*k5[0] + 5/66.*k6[0] + 0/1.*k7[0]) * Dt)
@@ -768,7 +772,7 @@ import ()
 //		p.tempm = norm(p.tempm)
 //
 //		//the error is the difference between the two solutions
-//		//error := math.Sqrt(sqr(p.m[0]-p.tempm[0]) + sqr(p.m[1]-p.tempm[1]) + sqr(p.m[2]-p.tempm[2]))
+//		//error := math.Sqrt(sqr(p.M[0]-p.tempm[0]) + sqr(p.M[1]-p.tempm[1]) + sqr(p.M[2]-p.tempm[2]))
 //		error := p.M.Sub(p.tempm).Norm()
 //
 //		//fmt.Println("error    :", error)
@@ -796,7 +800,7 @@ import ()
 //		temp := p.temp()
 //		p.tempfield = temp
 //		k1 := p.tau(temp)
-//		p.fehlk1 = k1
+//		p.k1 = k1
 //
 //		p.M[0] += k1[0] * 2 / 27. * Dt
 //		p.M[1] += k1[1] * 2 / 27. * Dt
@@ -810,9 +814,9 @@ import ()
 //	for _, p := range Lijst {
 //
 //		temp := p.tempfield
-//		k1 := p.fehlk1
+//		k1 := p.k1
 //		k2 := p.tau(temp)
-//		p.fehlk2 = k2
+//		p.k2 = k2
 //
 //		p.M = p.tempm
 //		p.M[0] += ((1/36.*k1[0] + 1/12.*k2[0]) * Dt)
@@ -825,10 +829,10 @@ import ()
 //	}
 //	for _, p := range Lijst {
 //		temp := p.tempfield
-//		k1 := p.fehlk1
-//		k2 := p.fehlk2
+//		k1 := p.k1
+//		k2 := p.k2
 //		k3 := p.tau(temp)
-//		p.fehlk3 = k3
+//		p.k3 = k3
 //
 //		p.M = p.tempm
 //		p.M[0] += ((1/24.*k1[0] + 0.*k2[0] + 1/8.*k3[0]) * Dt)
@@ -841,11 +845,11 @@ import ()
 //	}
 //	for _, p := range Lijst {
 //		temp := p.tempfield
-//		k1 := p.fehlk1
-//		k2 := p.fehlk2
-//		k3 := p.fehlk3
+//		k1 := p.k1
+//		k2 := p.k2
+//		k3 := p.k3
 //		k4 := p.tau(temp)
-//		p.fehlk4 = k4
+//		p.k4 = k4
 //
 //		p.M = p.tempm
 //		p.M[0] += ((5/12.*k1[0] + 0.*k2[0] - 25/16.*k3[0] + 25/16.*k4[0]) * Dt)
@@ -858,12 +862,12 @@ import ()
 //	}
 //	for _, p := range Lijst {
 //		temp := p.tempfield
-//		k1 := p.fehlk1
-//		k2 := p.fehlk2
-//		k3 := p.fehlk3
-//		k4 := p.fehlk4
+//		k1 := p.k1
+//		k2 := p.k2
+//		k3 := p.k3
+//		k4 := p.k4
 //		k5 := p.tau(temp)
-//		p.fehlk5 = k5
+//		p.k5 = k5
 //
 //		p.M = p.tempm
 //		p.M[0] += ((1/20.*k1[0] + 0.*k2[0] + 0.*k3[0] + 1/4.*k4[0] + 1/5.*k5[0]) * Dt)
@@ -877,13 +881,13 @@ import ()
 //
 //	for _, p := range Lijst {
 //		temp := p.tempfield
-//		k1 := p.fehlk1
-//		k2 := p.fehlk2
-//		k3 := p.fehlk3
-//		k4 := p.fehlk4
-//		k5 := p.fehlk5
+//		k1 := p.k1
+//		k2 := p.k2
+//		k3 := p.k3
+//		k4 := p.k4
+//		k5 := p.k5
 //		k6 := p.tau(temp)
-//		p.fehlk6 = k6
+//		p.k6 = k6
 //
 //		p.M = p.tempm
 //		p.M[0] += ((-25/108.*k1[0] + 0.*k2[0] + 0.*k3[0] + 125/108.*k4[0] - 65/27.*k5[0] + 125/54.*k6[0]) * Dt)
@@ -897,14 +901,14 @@ import ()
 //
 //	for _, p := range Lijst {
 //		temp := p.tempfield
-//		k1 := p.fehlk1
-//		k2 := p.fehlk2
-//		k3 := p.fehlk3
-//		k4 := p.fehlk4
-//		k5 := p.fehlk5
-//		k6 := p.fehlk6
+//		k1 := p.k1
+//		k2 := p.k2
+//		k3 := p.k3
+//		k4 := p.k4
+//		k5 := p.k5
+//		k6 := p.k6
 //		k7 := p.tau(temp)
-//		p.fehlk7 = k7
+//		p.k7 = k7
 //
 //		p.M = p.tempm
 //		p.M[0] += ((31/300.*k1[0] + 0.*k2[0] + 0.*k3[0] + 0.*k4[0] + 61/225.*k5[0] - 2/9.*k6[0] + +13/900.*k7[0]) * Dt)
@@ -917,15 +921,15 @@ import ()
 //	}
 //	for _, p := range Lijst {
 //		temp := p.tempfield
-//		k1 := p.fehlk1
-//		k2 := p.fehlk2
-//		k3 := p.fehlk3
-//		k4 := p.fehlk4
-//		k5 := p.fehlk5
-//		k6 := p.fehlk6
-//		k7 := p.fehlk7
+//		k1 := p.k1
+//		k2 := p.k2
+//		k3 := p.k3
+//		k4 := p.k4
+//		k5 := p.k5
+//		k6 := p.k6
+//		k7 := p.k7
 //		k8 := p.tau(temp)
-//		p.fehlk8 = k8
+//		p.k8 = k8
 //
 //		p.M = p.tempm
 //		p.M[0] += ((2.*k1[0] + 0.*k2[0] + 0.*k3[0] - 53/6.*k4[0] + 704/45.*k5[0] - 107/9.*k6[0] + 67/90.*k7[0] + 3.*k8[0]) * Dt)
@@ -938,16 +942,16 @@ import ()
 //	}
 //	for _, p := range Lijst {
 //		temp := p.tempfield
-//		k1 := p.fehlk1
-//		k2 := p.fehlk2
-//		k3 := p.fehlk3
-//		k4 := p.fehlk4
-//		k5 := p.fehlk5
-//		k6 := p.fehlk6
-//		k7 := p.fehlk7
-//		k8 := p.fehlk8
+//		k1 := p.k1
+//		k2 := p.k2
+//		k3 := p.k3
+//		k4 := p.k4
+//		k5 := p.k5
+//		k6 := p.k6
+//		k7 := p.k7
+//		k8 := p.k8
 //		k9 := p.tau(temp)
-//		p.fehlk9 = k9
+//		p.k9 = k9
 //
 //		p.M = p.tempm
 //		p.M[0] += ((-91/108.*k1[0] + 0.*k2[0] + 0.*k3[0] + 23/108.*k4[0] - 976/135.*k5[0] + 311/54.*k6[0] - 19/60.*k7[0] + 17/6.*k8[0] - 1/12.*k9[0]) * Dt)
@@ -960,17 +964,17 @@ import ()
 //	}
 //	for _, p := range Lijst {
 //		temp := p.tempfield
-//		k1 := p.fehlk1
-//		k2 := p.fehlk2
-//		k3 := p.fehlk3
-//		k4 := p.fehlk4
-//		k5 := p.fehlk5
-//		k6 := p.fehlk6
-//		k7 := p.fehlk7
-//		k8 := p.fehlk8
-//		k9 := p.fehlk9
+//		k1 := p.k1
+//		k2 := p.k2
+//		k3 := p.k3
+//		k4 := p.k4
+//		k5 := p.k5
+//		k6 := p.k6
+//		k7 := p.k7
+//		k8 := p.k8
+//		k9 := p.k9
 //		k10 := p.tau(temp)
-//		p.fehlk10 = k10
+//		p.k10 = k10
 //
 //		p.M = p.tempm
 //		p.M[0] += ((2383/4100.*k1[0] + 0.*k2[0] + 0.*k3[0] - 341/164.*k4[0] + 4496/1025.*k5[0] - 301/82.*k6[0] + 2133/4100.*k7[0] + 45/82.*k8[0] + 45/164.*k9[0] + 18/41.*k10[0]) * Dt)
@@ -983,18 +987,18 @@ import ()
 //	}
 //	for _, p := range Lijst {
 //		temp := p.tempfield
-//		k1 := p.fehlk1
-//		k2 := p.fehlk2
-//		k3 := p.fehlk3
-//		k4 := p.fehlk4
-//		k5 := p.fehlk5
-//		k6 := p.fehlk6
-//		k7 := p.fehlk7
-//		k8 := p.fehlk8
-//		k9 := p.fehlk9
-//		k10 := p.fehlk10
+//		k1 := p.k1
+//		k2 := p.k2
+//		k3 := p.k3
+//		k4 := p.k4
+//		k5 := p.k5
+//		k6 := p.k6
+//		k7 := p.k7
+//		k8 := p.k8
+//		k9 := p.k9
+//		k10 := p.k10
 //		k11 := p.tau(temp)
-//		p.fehlk11 = k11
+//		p.k11 = k11
 //
 //		p.M = p.tempm
 //		p.M[0] += ((3/205.*k1[0] + 0.*k2[0] + 0.*k3[0] + 0.*k4[0] + 0.*k5[0] - 6/41.*k6[0] - 3/205.*k7[0] - 3/41.*k8[0] + 3/41.*k9[0] + 6/41.*k10[0]) * Dt)
@@ -1007,18 +1011,18 @@ import ()
 //	}
 //	for _, p := range Lijst {
 //		temp := p.tempfield
-//		k1 := p.fehlk1
-//		k2 := p.fehlk2
-//		k3 := p.fehlk3
-//		k4 := p.fehlk4
-//		k5 := p.fehlk5
-//		k6 := p.fehlk6
-//		k7 := p.fehlk7
-//		k8 := p.fehlk8
-//		k9 := p.fehlk9
-//		k10 := p.fehlk10
+//		k1 := p.k1
+//		k2 := p.k2
+//		k3 := p.k3
+//		k4 := p.k4
+//		k5 := p.k5
+//		k6 := p.k6
+//		k7 := p.k7
+//		k8 := p.k8
+//		k9 := p.k9
+//		k10 := p.k10
 //		k12 := p.tau(temp)
-//		p.fehlk12 = k12
+//		p.k12 = k12
 //
 //		p.M = p.tempm
 //		p.M[0] += ((-1777/4100.*k1[0] + 0.*k2[0] + 0.*k3[0] - 341/164.*k4[0] + 4496/1025.*k5[0] - 289/82.*k6[0] + 2193/4100.*k7[0] + 51/82.*k8[0] + 33/164.*k9[0] + 12/41.*k10[0] + 1.*k12[0]) * Dt)
@@ -1028,16 +1032,16 @@ import ()
 //	}
 //	for _, p := range Lijst {
 //		temp := p.tempfield
-//		k1 := p.fehlk1
-//		k6 := p.fehlk6
-//		k7 := p.fehlk7
-//		k8 := p.fehlk8
-//		k9 := p.fehlk9
-//		k10 := p.fehlk10
-//		k11 := p.fehlk11
-//		k12 := p.fehlk12
+//		k1 := p.k1
+//		k6 := p.k6
+//		k7 := p.k7
+//		k8 := p.k8
+//		k9 := p.k9
+//		k10 := p.k10
+//		k11 := p.k11
+//		k12 := p.k12
 //		k13 := p.tau(temp)
-//		p.fehlk13 = k13
+//		p.k13 = k13
 //
 //		p.M = p.tempm
 //		p.tempm[0] += ((41/840.*k1[0] + 34/105.*k6[0] + 9/35.*k7[0] + 9/35.*k8[0] + 9/280.*k9[0] + 9/280.*k10[0] + 41/840.*k11[0]) * Dt)
@@ -1054,7 +1058,7 @@ import ()
 //		p.tempm = norm(p.tempm)
 //
 //		//the error is the difference between the two solutions
-//		//error := math.Sqrt(sqr(p.m[0]-p.tempm[0]) + sqr(p.m[1]-p.tempm[1]) + sqr(p.m[2]-p.tempm[2]))
+//		//error := math.Sqrt(sqr(p.M[0]-p.tempm[0]) + sqr(p.M[1]-p.tempm[1]) + sqr(p.M[2]-p.tempm[2]))
 //		error := p.M.Sub(p.tempm).Norm()
 //
 //		//fmt.Println("error    :", error)
