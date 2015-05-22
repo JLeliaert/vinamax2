af09146312e38adc281340690b6994f3f37b2d27
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2015-05-22 09:23:39 +0200

    derivatives start looking less wrong

--- dipole.go
+++ dipole.go
@@ -27,11 +27,9 @@ func DipoleField(m, R Vector) Vector {
 // Partial derivative (dB/di) of field generated at position R relative to dipole m.
 // Direction of derivative: i = X,Y or Z.
 
-// dBx/dx = 3*x*(m1*x+m2*y+m3*z)/(x^2+y^2+z^2)^(5/2) - m1/(x^2+y^2+z^2)^(3/2)
-// dBx/dy = -15xy mdotR / r^7 + 3y m_x/r^5 + 3m_y x / r^5
 func DiffDipole(i int, m, R Vector) Vector {
 
-	return Vector{d(X, i), d(Y, i), d(Z, i)}
+	return Vector{d(X, i, m, R), d(Y, i, m, R), d(Z, i, m, R)}
 
 	//r := R.Len()
 	//r2 := r * r
@@ -58,18 +56,29 @@ func DiffDipole(i int, m, R Vector) Vector {
 	//return Vector{dBxdi, dBydi, dBzdi}.Mul(1 / (4 * math.Pi))
 }
 
-func d(c, i int) float64 {
+func d(c, i int, m, R Vector) float64 {
 	if c == i {
-		return ddiag(i)
+		return ddiag(i, m, R)
 	} else {
-		return doff(c, i)
+		return doff(c, i, m, R)
 	}
 }
 
-func ddiag(i int) float64 {
-	return 0
+// dBx/dx = 3*x*(m1*x+m2*y+m3*z)/(x^2+y^2+z^2)^(5/2) - m1/(x^2+y^2+z^2)^(3/2)
+func ddiag(i int, m, R Vector) float64 {
+	x := R[i]
+	r2 := R.Dot(R)
+	pre := 1 / (4 * math.Pi)
+	return pre * (3*x*m.Dot(R)/math.Pow(r2, 5./2.) - m[i]/math.Pow(r2, 3./2.))
 }
 
-func doff(c, i int) float64 {
-	return 0
+// dBx/dy = -15xy mdotR / r^7 + 3y m_x/r^5 + 3m_y x / r^5
+func doff(c, i int, m, R Vector) float64 {
+	x := R[c]
+	y := R[i]
+	m_x := m[c]
+	m_y := m[i]
+	r2 := R.Dot(R)
+	pre := -1 / (4 * math.Pi) // TODO: result looks better with minus, what's wrong??
+	return pre * (-15*x*y*m.Dot(R)/math.Pow(r2, 7./2.) + 3*y*m_x/math.Pow(r2, 5./2.) + 3*m_y*x/math.Pow(r2, 5./2.))
 }
